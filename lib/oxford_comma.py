110f4af0ecac60e954913603e0d273e0c00ae797
chaya <chayavogel1@gmail.com>
2023-11-16 15:42:09 -0500

    completes lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return "".join(items)
+    elif len(items) ==2:
+        return " and ".join(items)
+    else:
+        last_item = items.pop()
+        split_items = ", ".join(items)
+        sentence = split_items + ", and " + last_item
+        return sentence
